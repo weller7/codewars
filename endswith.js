07a11bbc1d9e86a0416640b07c1389417ee14d84
Jim <jime1937@gmail.com>
2023-06-12 17:39:55 -0600

    New solution

---
+++ endswith.js
@@ -0,0 +1,20 @@
+// Task:
+// Complete the solution so that it returns true if the first 
+// argument(string) passed in ends with the 2nd argument (also a string).
+
+
+// Examples:
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+//psedudo code
+//convert to array check if they are the same
+
+
+//solution
+function solution(str, ending){ 
+    return str.endsWith(ending); 
+ }
+ 
+
+solution("abc, bc")
