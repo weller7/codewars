8d90eeab97ec027b7dee767cb516588fe00e4760
Jim <jime1937@gmail.com>
2023-07-18 16:04:50 -0600

    New challenge

---
+++ norepeat.js
@@ -0,0 +1,22 @@
+// Task:
+// Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+
+
+// Examples:
+// For input: [3, 4, 4, 3, 6, 3]
+
+//     remove the 3 at index 0
+//     remove the 4 at index 1
+//     remove the 3 at index 3
+
+// Expected output: [4, 6, 3]
+
+//psedudo code
+//use filter to check the array from right to left and check if the index is equal to the current elem
+//if not then exlude it
+
+
+//solution
+function solve(arr){
+    return arr.filter((val,i) => arr.lastIndexOf(val) == i);
+}
