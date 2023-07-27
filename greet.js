389f3a1c74f285c43401736f4a2f7ce3f038c198
Jim <jime1937@gmail.com>
2023-07-27 17:01:15 -0600

    Add

--- greet.js
+++ greet.js
@@ -14,4 +14,9 @@ var greet = function(name) {
     return "Hello " + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + "!";
   };
 
+
+  var greet = function(name) {
+    return `Hello ${name.slice(0,1).toUpperCase()}${name.slice(1).toLowerCase()}!`;
+  };
+
 greet("ominis")
