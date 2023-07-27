39a3efd5c449ae9a60131ba1675f8d7483309e87
Jim <jime1937@gmail.com>
2023-07-27 16:58:11 -0600

    New challenge

---
+++ greet.js
@@ -0,0 +1,17 @@
+// Task
+
+
+// Examples:
+
+
+//psedudo code
+//
+
+
+
+//solution
+var greet = function(name) {
+    return "Hello " + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + "!";
+  };
+
+greet("ominis")
