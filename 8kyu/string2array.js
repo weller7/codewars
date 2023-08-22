752f88517649cdaf3ab92f6c096c762843851b00
Jim <jime1937@gmail.com>
2023-08-22 16:54:06 -0600

    new challenge

---
+++ 8kyu/string2array.js
@@ -0,0 +1,17 @@
+// Task
+// Write a function to split a string and convert it into an array of words.
+
+
+// Examples:
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love"]
+
+//psedudo code
+//use split to to make the strings into arrays
+
+//solution
+function stringToArray (string){
+    console.log(string.split(" "))
+}
+stringToArray ("hello ominis gaunt")
