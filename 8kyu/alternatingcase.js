da437113748e11a40860c4ab038937cfe145503f
Jim <jime1937@gmail.com>
2024-06-06 16:02:09 -0600

    New challenge

---
+++ 8kyu/alternatingcase.js
@@ -0,0 +1,30 @@
+// Task
+// Define String.prototype.toAlternatingCase (or a similar function/method such 
+// as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; 
+// see the initial solution for details) such that each lowercase letter becomes uppercase 
+// and each uppercase letter becomes lowercase. For example:
+
+
+
+// // Examples:
+// "hello world".toAlternatingCase() === "HELLO WORLD"
+// "HELLO WORLD".toAlternatingCase() === "hello world"
+// "hello WORLD".toAlternatingCase() === "HELLO world"
+// "HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+// "12345".toAlternatingCase()       === "12345"                   // Non-alphabetical characters are unaffected
+// "1a2b3c4d5e".toAlternatingCase()  === "1A2B3C4D5E"
+// "String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+
+// As usual, your function/method should be pure, i.e. it should not mutate the original string.
+
+//psedudo code
+//
+
+
+
+//solution
+String.prototype.toAlternatingCase = function () {
+   return this.split("").map(letter => letter === letter.toUpperCase()? letter.toLowerCase() :letter.toUpperCase()).join("")
+  }
+
+  "hello world".toAlternatingCase()
