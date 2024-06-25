30346fe9870e370d10f7203dde1fca1f980880fe
Jim <jime1937@gmail.com>
2024-06-25 15:49:43 -0600

    New challenge

---
+++ 8kyu/squaring.js
@@ -0,0 +1,16 @@
+// Task
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// Examples:
+// assert.strictEqual(square(3), 9);
+
+//psedudo code
+//
+
+
+
+//solution
+function square (num){
+    console.log((num **2))
+}
+square (3)
