32a0fa62dabe6737b01cbfd903c015bfbd256590
Jimena De Lara <40869710+weller7@users.noreply.github.com>
2025-04-07 11:03:42 -0600

    Update 123testing.js

--- 123testing.js
+++ 123testing.js
@@ -1,4 +1,4 @@
-// TasK
+// Task
 // Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
 // Write a function which takes a list of strings and returns each line prepended by the correct number.
 // The numbering starts at 1. The format is n: string. Notice the colon and space in between
